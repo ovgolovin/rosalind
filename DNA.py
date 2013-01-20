7d022e6c591461047c88d71bb6e6f5d2d2217629
ovgolovin <ovgolovin@gmail.com>
2013-01-20 17:35:20 +0400

    DNA

---
+++ DNA.py
@@ -0,0 +1,28 @@
+#!usr/bin/env python2
+
+from collections import Counter
+import sys
+
+order = 'ACGT'
+
+def get_ordered_counts(input):
+    counts = Counter(input)
+    return ' '.join(str(counts.get(letter)) for letter in order)
+
+def test():
+    test_string = 'AGCTTTTCATTCTGACTGCAACGGGCAATATGTCTCTGTGTGGATTAAAAAAAGAGTGTCTGATAGCAGC'
+    result = get_ordered_counts(test_string)
+    assert result == '20 12 17 21'
+    print(result)
+    print('Test passed!')
+
+def main():
+    if len(sys.argv) > 1:
+        filename = sys.argv[1]
+        with open(filename) as file:
+            print(get_ordered_counts(file.read()))
+    else:
+        test()
+
+if __name__ == '__main__':
+    main()
